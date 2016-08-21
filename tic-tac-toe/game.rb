2ce88697c6760747a0306862ff3f8dedc9b62f82
Joanna Takesian <joanna.takesian@gmail.com>
2016-08-21 08:31:07 -0700

    updated tic tac toe files to check winner

--- tic-tac-toe/game.rb
+++ tic-tac-toe/game.rb
@@ -37,26 +37,40 @@ playing = true
 turns = 1
 players = [player_one, player_two]
 moves = [" "," "," "," "," "," "," "," "," "]
-LINES = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+lines = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+first, second, third = 0
 
 puts board(moves)
 
 while turns < 5 do
     players.each do |player|
         
-        puts "#{player.name()}'s turn!"
-        
-        puts "Enter a square 1-9:"
-        choice = gets.chomp.to_i
-        
-        if moves[choice - 1] != " "
-            puts "That move is taken. Please try again."
+        if turns < 6
+            puts "#{player.name()}'s turn!"
+            puts "Enter a square 1-9:"
             choice = gets.chomp.to_i
+        
+            if moves[choice - 1] != " "
+                puts "That move is taken. Please try again."
+                choice = gets.chomp.to_i
+            end
+        
+            moves[choice - 1] = player.symbol()
+        
+            puts board(moves)
+        
+            lines.each do |line|
+                first = line[0].to_i
+                second = line[1].to_i
+                third = line[2].to_i
+            
+                if moves[first] == player.symbol() && moves[second] == player.symbol() && moves[third] == player.symbol()
+                    puts "Game over!"
+                    puts "#{player.name()} wins!"
+                    turns += 5
+                end
+            end
         end
-        
-        moves[choice - 1] = player.symbol()
-        
-        puts board(moves)
         
     end
     
@@ -71,5 +85,23 @@ if turns == 5
         end
     end
     board(moves)
-    puts "end of game"
+    puts "Game over."
+    
+    wins = 0
+    
+    lines.each do |line|
+        first = line[0].to_i
+        second = line[1].to_i
+        third = line[2].to_i
+        
+        if moves[first] == "X" && moves[second] == "X" && moves[third] == "X"
+            wins += 1
+        end
+    end
+    
+    if wins > 0
+        puts "#{player_one.name()} wins!"
+    else
+        puts "It's a draw!"
+    end
 end
